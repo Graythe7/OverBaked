a97a08cd3be48be16555b531af5e14c3dc8e874c
Graythe7 <109918660+Graythe7@users.noreply.github.com>
2025-04-12 15:08:44 +0330

    player movement

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public Rigidbody2D rb;
+
+    private Vector2 movement;
+
+    private void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
